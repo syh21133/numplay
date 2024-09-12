4a0b4b4ffbd5bd77bd908bbd09ad0389beb820b3
syh213 <syh21133@gmail.com>
2024-09-12 14:15:43 +0900

    도전과제 수정

--- src/com/example/numplay/Main.java
+++ src/com/example/numplay/Main.java
@@ -15,23 +15,52 @@ public class Main {
         List<Integer> platNum = new ArrayList<>();
 
         String result = "";
+        boolean exit = true;
 
-        List<Integer> ranNum = randomNum.create();
-        System.out.println(ranNum);
-        while (!result.equals("3스트라이크")) {
 
-            System.out.println("숫자를 입력해주세요.");
-            String input = scanner.next();
+        while(exit){
 
-            if(Inputnum.isVaild(input)){
-                continue;
-            };
+            System.out.println("환영합니다! 원하시는 번호를 입력해주세요.");
+            System.out.println("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기");
+            int num = scanner.nextInt();
 
-            result = numResult.result(ranNum, inputnum.playerNumber(input));
+            switch(num) {
 
-            System.out.println(result);
+                case 1:
+
+                List<Integer> ranNum = randomNum.create();
+                System.out.println(ranNum);
+                while (!result.equals("3스트라이크")) {
+
+                    System.out.println("숫자를 입력해주세요.");
+                    String input = scanner.next();
+
+                    if (Inputnum.isVaild(input)) {
+                        continue;
+                    }
+                    ;
+
+                    result = numResult.result(ranNum, inputnum.playerNumber(input));
+                    if(!result.equals("3스트라이크")){
+                    System.out.println(result);}
+                }
+                result = "";
+                break;
+
+                case 2:
+                    // 기록보기
+                    numResult.showResult();
+                    break;
+                case 3:
+                    System.out.println("프로그램을 종료합니다.");
+                    exit=false;
+                    break;
+                default:
+                    System.out.println("잘못 입력하셨습니다. 다시 입력해 주세요.");
+
+            }
+
+
         }
-
-
     }
 }
--- src/com/example/numplay/Result.java
+++ src/com/example/numplay/Result.java
@@ -1,10 +1,14 @@
 package com.example.numplay;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Result {
 
     Compare compare = new Compare();
+    int count = 0;
+
+    List<Integer> gameResults = new ArrayList<>();
 
     public String result(List<Integer> computer, List<Integer> player) {
         int total = compare.total(computer, player);
@@ -12,18 +16,39 @@ public class Result {
         int ball = total - strike;
 
         if (total == 0) {
-            return "낫싱";
+            count += 1;
+            return "아웃";
         } else if (strike == 0) {
+            count += 1;
             return ball + "볼";
         } else if (strike == 3) {
+            count += 1;
             System.out.println("정답입니다. 축하합니다!");
+
+            addResult(count);
+
+            count = 0;
+            return strike + "스트라이크";
         } else if (ball == 0) {
+            count += 1;
             return strike + "스트라이크";
         }
 
+        count += 1;
+        return strike + "스트라이크 " + ball + "볼 ";
+    }
 
-        return strike + "스트라이크" + ball + "볼 ";
+    private void addResult(int count) {
+
+        gameResults.add(count);
+
     }
 
 
+    public void showResult() {
+        for(int i = 0 ; i < gameResults.size() ; i++) {
+
+            System.out.println((i+1)+"번째 게임 : 시도 횟수 - "+gameResults.get(i));
+        }
+    }
 }
